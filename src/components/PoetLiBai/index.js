7cba21b00e510566a1e5748c4b1bba458d3cc3b0
leforyou <1147724499@qq.com>
2018-12-08 13:34:06 +0800

    修改

--- src/components/PoetLiBai/index.js
+++ src/components/PoetLiBai/index.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 import "./style.scss";
 
-import { Router, Route , Link,NavLink,BrowserRouter, HashRouter, Switch } from 'react-router-dom';
+import { Route ,NavLink, Switch } from 'react-router-dom';
 
 import LiBai1 from './../LiBai1';
 import LiBai2 from './../LiBai2';
@@ -10,7 +10,8 @@ import LiBai3 from './../LiBai3';
 class index extends Component {
   constructor(props){
     super(props);
-    let url = this.props.match.path.replace(/\:id/,"");
+    let url = this.props.match.path;
+    url = url.replace(/\/:id/,"");
     this.state = {
       url:url
     }
@@ -22,13 +23,12 @@ class index extends Component {
     return (
       <div className="poet-content">
         <div className="titles">
-          <NavLink activeClassName="selected" to={`${this.state.url}LiBai1`}>《望庐山瀑布》</NavLink>
+          <NavLink activeClassName="selected" to={`${this.state.url}/LiBai1`}>《望庐山瀑布》</NavLink>
           <NavLink activeClassName="selected" to="/PoetLiBai/LiBai2">《早发白帝城》</NavLink>
           <NavLink activeClassName="selected" to="/PoetLiBai/LiBai3">《赠汪伦》</NavLink>
           <span>二级路由</span>
         </div>
         <div className="box">
-          {this.props.children /** 二级路由的内容位置 */}
           <Switch>
               <Route exact path="/PoetLiBai/LiBai1" component={LiBai1}/>
               <Route exact path="/PoetLiBai/LiBai2" component={LiBai2}/>
